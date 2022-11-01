20589b2fc5e8112f6f902e188db9ade71244a8c3
flyingdelta <flyingdelta@outlook.com>
2022-11-01 19:54:15 +0530

    Changed notes to store objects with ids as keys

--- components/NotesManager.js
+++ components/NotesManager.js
@@ -4,15 +4,21 @@ import NotesToolbar from "./NotesToolbar";
 import styles from "./NotesManager.module.css";
 
 export default function NotesManager() {
-  let [notes, setNotes] = useState([]);
+  let objectsLength = useRef(0);
+  let [notes, setNotes] = useState({});
   function addNote() {
-    setNotes(notes.concat({ id: `note-${notes.length}` }));
+    notes[`note-${++objectsLength.current}`] = {};
+    setNotes({ ...notes });
+  }
+  function deleteNote(id) {
+    delete notes[id];
+    setNotes({ ...notes });
   }
   return (
     <div className={`notes-manager ${styles.notesManager}`}>
       <NotesToolbar handleAddNote={addNote} />
-      {notes.map((note) => {
-        return <Note key={note.id} id={note.id} />;
+      {Object.keys(notes).map((noteId) => {
+        return <Note key={noteId} id={noteId} onDelete={deleteNote} />;
       })}
     </div>
   );
