09f6268be707f2eaf2d4918a41b0c9dc72d6f41e
flyingdelta <flyingdelta@outlook.com>
2022-11-05 17:48:20 +0530

    Added RelativeContainer for init position of notes

--- components/Note.js
+++ components/Note.js
@@ -39,13 +39,15 @@ const MoveDiv = styled("div", {
   },
 });
 
-const HoverCard = styled(Card, {
+const RelativeContainer = styled(Card, {
   zIndex: "initial !important",
-  position: "relative !important",
+  position: "relative",
   padding: "20px 10px 10px 10px !important",
   display: "grid !important",
   gridTemplateRows: "max-content minmax(0, 1fr)",
   gap: "5px 0px",
+  height: "100%",
+  width: "100% !important",
   "@bp3": {
     padding: "60px 10px 10px 10px !important",
     msTouchAction: "none",
@@ -81,6 +83,20 @@ const HoverCard = styled(Card, {
       zIndex: "initial",
     },
   },
+});
+
+const AbsoluteContainer = styled(Container, {
+  position: "absolute",
+  top: 0,
+  bottom: 0,
+  width: "fit-content !important",
+  height: "fit-content",
+});
+
+const HoverCard = styled(Card, {
+  position: "absolute !important",
+  width: "fit-content",
+  height: "fit-content",
 });
 
 const DynamicEditor = dynamic(() => import("./QuillEditor"), {
@@ -189,83 +205,89 @@ const Note = React.memo(function MemoNote({ id, onDelete, onSave }) {
       className="draggable note"
       ref={noteElement}
     >
-      <MoveDiv className="moveBar"></MoveDiv>
-      <Suspense fallback={`Loading...`}>
-        <Card.Header>
-          <DynamicEditor
-            id={`${id}-note-title`}
-            selector={`#${id}-note-title`}
-            className="note-title"
-            placeholder="Title"
-            toolbar={false}
-            camouflage={true}
-          />
-        </Card.Header>
-        <Card.Content>
-          <DynamicEditor
-            id={`${id}-note-body`}
-            selector={`#${id}-note-body`}
-            className="note-body scrolling-container"
-            scrollContainer={`#${id}.scrolling-container`}
-          />
-        </Card.Content>
-      </Suspense>
+      <RelativeContainer>
+        <MoveDiv className="moveBar"></MoveDiv>
+        <Suspense fallback={`Loading...`}>
+          <Card.Header>
+            <DynamicEditor
+              id={`${id}-note-title`}
+              selector={`#${id}-note-title`}
+              className="note-title"
+              placeholder="Title"
+              toolbar={false}
+              camouflage={true}
+            />
+          </Card.Header>
+          <Card.Content>
+            <DynamicEditor
+              id={`${id}-note-body`}
+              selector={`#${id}-note-body`}
+              className="note-body scrolling-container"
+              scrollContainer={`#${id}.scrolling-container`}
+            />
+          </Card.Content>
+        </Suspense>
 
-      <ResizeBtn tabIndex="0" className="resizeBtn moreSpecific">
-        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24">
-          <path
-            fill="#001A72"
-            d="M21 12a9 9 0 11-18 0 9 9 0 0118 0z"
-            opacity="0.15"
-          ></path>
-          <path
-            fill="#001A72"
-            d="M14.47 10.53a.75.75 0 101.06-1.06l-1.06 1.06zM12 7l.53-.53a.75.75 0 00-1.06 0L12 7zM8.47 9.47a.75.75 0 101.06 1.06L8.47 9.47zm1.06 4a.75.75 0 00-1.06 1.06l1.06-1.06zM12 17l-.53.53a.75.75 0 001.06 0L12 17zm3.53-2.47a.75.75 0 10-1.06-1.06l1.06 1.06zm0-5.06l-3-3-1.06 1.06 3 3 1.06-1.06zm-4.06-3l-3 3 1.06 1.06 3-3-1.06-1.06zm-3 8.06l3 3 1.06-1.06-3-3-1.06 1.06zm4.06 3l3-3-1.06-1.06-3 3 1.06 1.06zM20.25 12A8.25 8.25 0 0112 20.25v1.5c5.385 0 9.75-4.365 9.75-9.75h-1.5zM12 20.25A8.25 8.25 0 013.75 12h-1.5c0 5.385 4.365 9.75 9.75 9.75v-1.5zM3.75 12A8.25 8.25 0 0112 3.75v-1.5c-5.385 0-9.75 4.365-9.75 9.75h1.5zM12 3.75A8.25 8.25 0 0120.25 12h1.5c0-5.385-4.365-9.75-9.75-9.75v1.5z"
-          ></path>
-        </svg>
-      </ResizeBtn>
-      <PopupMenu
-        body={
-          <Grid>
-            <Grid.Row>
-              <Grid.Column>
-                <Notification
-                  buttonText="Save"
-                  fluid
-                  icon={<Icon name="save" />}
-                  labelPosition="left"
-                  onTrigger={() => {}}
-                  toast={{
-                    title: "Saved!",
-                    description: "Saved Successfully",
-                    actionBtn: <Button>Undo</Button>,
-                  }}
-                />
-              </Grid.Column>
-            </Grid.Row>
-            <Grid.Row>
-              <Grid.Column>
-                <Modal
-                  openTrigger={
-                    <Button icon labelPosition="left" negative fluid>
-                      <Icon name="delete"></Icon>
-                      Delete
-                    </Button>
-                  }
-                  closeTrigger={<Button negative>delete</Button>}
-                  title="Are you sure?"
-                  body={
-                    <Container>
-                      Are you sure you want to delete the note?
-                    </Container>
-                  }
-                  onClick={handleDelete}
-                />
-              </Grid.Column>
-            </Grid.Row>
-          </Grid>
-        }
-      />
+        <ResizeBtn tabIndex="0" className="resizeBtn moreSpecific">
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 24 24"
+          >
+            <path
+              fill="#001A72"
+              d="M21 12a9 9 0 11-18 0 9 9 0 0118 0z"
+              opacity="0.15"
+            ></path>
+            <path
+              fill="#001A72"
+              d="M14.47 10.53a.75.75 0 101.06-1.06l-1.06 1.06zM12 7l.53-.53a.75.75 0 00-1.06 0L12 7zM8.47 9.47a.75.75 0 101.06 1.06L8.47 9.47zm1.06 4a.75.75 0 00-1.06 1.06l1.06-1.06zM12 17l-.53.53a.75.75 0 001.06 0L12 17zm3.53-2.47a.75.75 0 10-1.06-1.06l1.06 1.06zm0-5.06l-3-3-1.06 1.06 3 3 1.06-1.06zm-4.06-3l-3 3 1.06 1.06 3-3-1.06-1.06zm-3 8.06l3 3 1.06-1.06-3-3-1.06 1.06zm4.06 3l3-3-1.06-1.06-3 3 1.06 1.06zM20.25 12A8.25 8.25 0 0112 20.25v1.5c5.385 0 9.75-4.365 9.75-9.75h-1.5zM12 20.25A8.25 8.25 0 013.75 12h-1.5c0 5.385 4.365 9.75 9.75 9.75v-1.5zM3.75 12A8.25 8.25 0 0112 3.75v-1.5c-5.385 0-9.75 4.365-9.75 9.75h1.5zM12 3.75A8.25 8.25 0 0120.25 12h1.5c0-5.385-4.365-9.75-9.75-9.75v1.5z"
+            ></path>
+          </svg>
+        </ResizeBtn>
+        <PopupMenu
+          body={
+            <Grid>
+              <Grid.Row>
+                <Grid.Column>
+                  <Notification
+                    buttonText="Save"
+                    fluid
+                    icon={<Icon name="save" />}
+                    labelPosition="left"
+                    onTrigger={() => {}}
+                    toast={{
+                      title: "Saved!",
+                      description: "Saved Successfully",
+                      actionBtn: <Button>Undo</Button>,
+                    }}
+                  />
+                </Grid.Column>
+              </Grid.Row>
+              <Grid.Row>
+                <Grid.Column>
+                  <Modal
+                    openTrigger={
+                      <Button icon labelPosition="left" negative fluid>
+                        <Icon name="delete"></Icon>
+                        Delete
+                      </Button>
+                    }
+                    closeTrigger={<Button negative>delete</Button>}
+                    title="Are you sure?"
+                    body={
+                      <Container>
+                        Are you sure you want to delete the note?
+                      </Container>
+                    }
+                    onClick={handleDelete}
+                  />
+                </Grid.Column>
+              </Grid.Row>
+            </Grid>
+          }
+        />
+      </RelativeContainer>
     </HoverCard>
   );
 });
