73324b1f48eeb8205a1d659e9e3008c1b3ebe172
flyingdelta <flyingdelta@outlook.com>
2022-11-05 12:30:25 +0530

    Added Portal Component to go with Toast.ViewPort

---
+++ components/ReactPortal.js
@@ -0,0 +1,19 @@
+import { createPortal } from "react-dom";
+
+function createWrapperAndAppendToBody(wrapperId) {
+  const wrapperElement = document.createElement("div");
+  wrapperElement.setAttribute("id", wrapperId);
+  document.body.appendChild(wrapperElement);
+  return wrapperElement;
+}
+
+export default function Portal({ children, wrapperId = "portals" }) {
+  let element = document.getElementById(wrapperId);
+  // if element is not found with wrapperId,
+  // create and append to body
+  if (!element) {
+    element = createWrapperAndAppendToBody(wrapperId);
+  }
+
+  return createPortal(children, element);
+}
